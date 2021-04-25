138f1983055fab138d605ef011e111d5f22d9fb7
Joaquim Neto <joaquimmy@gmail.com>
2021-04-25 17:59:53 -0300

    uncomment web stuff

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const { Logger } = require('./shared');
 Logger.info(`Spooney v${require('../package.json').version}`);
 Logger.info(`Running on Node ${process.version}`);
 
-// const web = require('./web');
+const web = require('./web');
 const bot = require('./bot');
 
-// web();
+web();
 bot();
